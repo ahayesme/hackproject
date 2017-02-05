09dceba37fc7953649836eed13054597c317d4df
ahayesme <amani_hayes-messinger@brown.edu>
2017-02-04 21:12:21 -0500

    idk

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,8 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
+import '/imports/ui/'
+
 FlowRouter.route('/', {
   action: function() {
     BlazeLayout.render("mainLayout", {content: "Homepage"});
